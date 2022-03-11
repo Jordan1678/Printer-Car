a42e450c4d6f9aa64c23c8da03b87057ab29a3aa
Jordan <87206931+Jordan1678@users.noreply.github.com>
2022-03-11 08:41:16 -0500

    added a buzzer
    added a buzzer to make annoy people

--- Printer_Car_Main.ino
+++ Printer_Car_Main.ino
@@ -6,11 +6,13 @@
 //define the pins
 #define LeftMotor 13
 #define RightMotor 12
+#define Buzzer 11
 
 void setup() {
   // setting pin modes to OUTPUT where needed
   pinMode(LeftMotor, OUTPUT);
   pinMode(RightMotor, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
 
   // set motor pins high and never shuts them off
   digitalWrite(LeftMotor, HIGH);
@@ -18,5 +20,12 @@ void setup() {
   
 }
 
-// Nothing in loop because nothing is needed
-void loop() {}
+// tone is an internal command for buzzers to make music
+// noTone stops all tones
+void loop() {
+  tone(Buzzer, 1000);
+  delay(1000);
+  noTone(Buzzer);
+  delay(1000);
+  
+}
