e4e13578b9840c88ebb799b27938d07dab94a193
Jordan <87206931+Jordan1678@users.noreply.github.com>
2022-03-11 07:58:30 -0500

    Final Code
    removed everything that was unnecessary

--- Printer_Car_Main.ino
+++ Printer_Car_Main.ino
@@ -6,49 +6,17 @@
 //define the pins
 #define LeftMotor 13
 #define RightMotor 12
-#define Buzzer 11
-#define Button 10
-
-// define an int to read the button 
-int ReadButton;
-
-// define a bool to turn on and off motors
-boolean MotorOn = false;
 
 void setup() {
-  // setting pin modes to OUTPUT or INPUT where needed
+  // setting pin modes to OUTPUT where needed
   pinMode(LeftMotor, OUTPUT);
   pinMode(RightMotor, OUTPUT);
-  pinMode(Buzzer, OUTPUT);
-  pinMode(Button, INPUT);
 
-  // starts the serial outputs for debuging
-  Serial.begin(9600);
-
+  // set motor pins high and never shuts them off
+  digitalWrite(LeftMotor, HIGH);
+  digitalWrite(RightMotor, HIGH);
+  
 }
 
-void loop() {
-  // reads input from button pin
-  ReadButton = digitalRead(Button);
-
-  // takes a the input from button pin checks if its HIGH
-  // and if so it will turn motors on if not it will turn them off
-  if (ReadButton == HIGH) {
-    MotorOn = true;
-
-  } else {
-    MotorOn = false;
-
-  }
-
-  if (MotorOn) {
-    digitalWrite(LeftMotor, HIGH);
-    digitalWrite(LeftMotor, HIGH);
-
-  } else {
-    digitalWrite(LeftMotor, LOW);
-    digitalWrite(LeftMotor, LOW);
-
-  }
-
-}
+// Nothing in loop because nothing is needed
+void loop() {}
